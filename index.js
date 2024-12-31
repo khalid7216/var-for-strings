22d13800139aab892c187ee0ea9cc055528faf05
khalid7216 <156210757+khalid7216@users.noreply.github.com>
2024-12-31 05:42:12 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let firstname = "khalid"
+let lastname = "Sanawer"
+let age = 18;
+let country = "Pakistan";
+let city = "Karachi";
+let occupation = "Student";
+alert(firstname);
+alert(lastname);
+alert(city);
+alert(country);
+alert(occupation)
+alert(age)
+let cert = "certified Mobile App Developer";
+alert(cert);
+let email = "khalid@mail.com";
+alert(email);
+let message =  "pizza\npizz\npiz\npi\np"
+alert(message);
