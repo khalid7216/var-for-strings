bfd2ebaaba1c9740b74d4db2dffb23473b97fced
khalid7216 <156210757+khalid7216@users.noreply.github.com>
2024-12-31 05:04:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstname = "khalid"
+let lastname = "Sanawer"
+let age = 18;
+let country = "Pakistan";
+let city = "Karachi";
+let occupation = "Student";
+alert(firstname);
+alert(lastname);
+alert(city);
+alert(country);
+alert(occupation)
+alert(age)
